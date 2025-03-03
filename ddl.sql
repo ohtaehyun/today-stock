fc0aef824bdff5a46c68f2b8811c0e838bf7d87a
ohtaehyun <othdev95@gmail.com>
2025-03-03 18:30:17 +0900

    feat: 주가 기록용 테이블 추가

--- ddl.sql
+++ ddl.sql
@@ -6,3 +6,23 @@ CREATE TABLE fear_greed
   score int NOT NULL,
   created_at timestamptz NOT NULL DEFAULT NOW()
 );
+
+CREATE TABLE ticker
+(
+  ticker_id serial4 PRIMARY KEY,
+  ticker varchar NOT NULL,
+  created_at timestamptz NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE stock_history
+(
+  ticker_id int4 NOT NULL,
+  trade_date timestamptz NOT NULL,
+  candle_type int NOT NULL,
+  open_price numeric NOT NULL,
+  high_price numeric NOT NULL,
+  low_price numeric NOT NULL,
+  close_price numeric NOT NULL,
+  volume int8 NOT NULL,
+  constraint stock_history_pk PRIMARY KEY(ticker_id, trade_date, candle_type)
+);
